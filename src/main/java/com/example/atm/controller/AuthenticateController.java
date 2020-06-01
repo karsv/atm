d0e0660580863f1e5501eebee73da508443ca68c
karsv <Pbvf2019!>
2020-06-01 11:04:52 +0300

    fix bug while registration

--- src/main/java/com/example/atm/controller/AuthenticateController.java
+++ src/main/java/com/example/atm/controller/AuthenticateController.java
@@ -6,12 +6,20 @@ import com.example.atm.dto.PersonResponseDto;
 import com.example.atm.exception.AuthenticateException;
 import com.example.atm.model.Person;
 import com.example.atm.service.AuthenticateService;
+import java.util.List;
 import javax.naming.AuthenticationException;
 import javax.validation.Valid;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -42,4 +50,10 @@ public class AuthenticateController {
                 .orElseThrow(() -> new AuthenticateException());
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public AuthenticateException validationError(MethodArgumentNotValidException exception) {
+        String message =exception.getBindingResult().getFieldErrors().get(1).getDefaultMessage();
+        return new AuthenticateException(message);
+    }
 }
